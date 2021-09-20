ed20d6883bd622ee17dd623569ec5850acc9176c
dev <dev@devs.local>
2021-09-20 21:20:46 +0300

    -

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -65,18 +65,21 @@ def create_city(state_id):
     return jsonify(new_city.to_dict()), 201
 
 
-@app_views.route(
-    '/cities/<city_id>',
-    methods=['PUT'],
-    strict_slashes=False)
-def put_city(city_id):
-    """Updates an existing city"""
-    city_dict = request.get_json()
-    if not city_dict:
-        return (jsonify({'error': 'Not a JSON'}), 400)
-    city = storage.get('City', city_id)
-    if city:
-        city.name = city_dict['name']
-        city.save()
-        return (jsonify(city.to_dict()), 200)
-    abort(404)
+@app_views.route("/cities/<city_id>", methods=["PUT"], strict_slashes=False)
+def update_city(city_id):
+    """ Updates a city """
+    city = storage.get("City", str(city_id))
+
+    if city is None:
+        abort(404)
+
+    city_obj = request.get_json(silent=True)
+
+    if city_obj is None:
+        abort(400, "Not a json")
+
+    for k, v in city_obj.items():
+        if k not in ["id", "created_at", "updated_at"]:
+            setattr(city, k, v)
+    city.save()
+    return jsonify(city.to_dict()), 200
